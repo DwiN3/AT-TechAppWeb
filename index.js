545cbd083dca5da86d03e767379b072cb42c714c
DwiN3 <dwinicht333@gmail.com>
2024-03-19 10:13:25 +0100

    dodanie package.json

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer();
-server.on('connection', (socket => {
-console.log('new connection');
-}));
-
-server.listen(3000);
-console.info('Server is running on port 3000');
