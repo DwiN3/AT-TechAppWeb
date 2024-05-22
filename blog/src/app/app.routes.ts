67f51b3252d8dd6b2a7fae48fb83b86533c884f5
DwiN3 <dwinicht333@gmail.com>
2024-05-22 18:22:44 +0200

    lab-10-zad-3 (nadal error)

--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { BlogComponent } from "./components/blog/blog.component";
+import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
 
 export const routes: Routes = [
     {
-        path: '',
-        component: BlogComponent,
-    }
+        path: 'blog/detail/:id',
+        component: BlogItemDetailsComponent
+    }        
 ];
