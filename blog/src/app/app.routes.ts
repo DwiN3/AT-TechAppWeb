84cc72ea11f51936eb818c93280bdad67c21f50d
DwiN3 <dwinicht333@gmail.com>
2024-06-14 14:33:48 +0200

    lab-10-zad-1

--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { BlogHomeComponent } from './components/blog-home/blog-home.component';
 import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: BlogHomeComponent,
+        component: HomeComponent
+    },
+    {
+        path: 'blog',
+        component: BlogHomeComponent
     },
     {
         path: 'blog/detail/:id',
-        component: BlogItemDetailsComponent,
-    }       
+        component: BlogItemDetailsComponent
+    }
 ];
