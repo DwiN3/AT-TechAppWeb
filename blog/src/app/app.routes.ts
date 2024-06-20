a4892d1d861658f0fea7437e0c575cf1d7aeca44
DwiN3 <dwinicht333@gmail.com>
2024-06-20 11:45:49 +0200

    dodanie strażnika dla zalogowanych użytkownikow

--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './services/auth/auth.guard';
+import { guestGuard } from './services/guest/guest.guard';
 
 export const routes: Routes = [
   {
@@ -8,11 +9,13 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
+    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent),
+    canActivate: [guestGuard]
   },
   {
     path: 'signup',
-    loadComponent: () => import('./components/signup/signup.component').then(m => m.SignupComponent)
+    loadComponent: () => import('./components/signup/signup.component').then(m => m.SignupComponent),
+    canActivate: [guestGuard]
   },
   {
     path: 'blog',
